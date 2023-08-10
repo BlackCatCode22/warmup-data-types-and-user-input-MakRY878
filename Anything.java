98ba8381a5567a4088bc4b2db1b1ed9260150a2d
MakRY878 <141769737+MakRY878@users.noreply.github.com>
2023-08-10 12:19:25 -0700

    warmup10aug

---
+++ Anything.java
@@ -0,0 +1,21 @@
+//use the scanner class
+import java.util.Scanner;
+
+public class Anything {
+    public static void main(String[] args) {
+    int num1 = 0; int num2 = 0;int num3 = 0;int largestnum = 0;
+        System.out.println("\n\n Welcome to the largest numbers program!\n\n");
+
+        Scanner scanner = new Scanner(System.in);
+    //prompt the user for the first number.
+    System.out.println("\n Please enter the first number: ");
+    int  = scanner.nextInt(); System.out.println("\n You entered: " + num1);
+    System.out.println("\n Please enter the second number: ");
+        int  = scanner.nextInt(); System.out.println("\n You entered: " + num2);
+     System.out.println("\n Please enter the third number: ");
+        int  = scanner.nextInt();  System.out.println("\n You entered: " + num3);
+
+
+    System.out.println("\n The numbers are :" + num1  +num2  + num3);    }
+   //find the largest number
+    public static int findlarge
